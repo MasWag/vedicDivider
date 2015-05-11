f47070d53b3ccecef1352c0ceb4493396bb74b88
Masaki Waga <masakiwaga@gmail.com>
2015-05-11 16:31:40 +0900

    testを増やした

--- test_gen32.sh
+++ test_gen32.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for a in $(seq 0 4095); do
+for a in $(seq 0 32767); do
     d0=$RANDOM;
-    d1=$RANDOM;
+    d1=$(($RANDOM + 1));
     printf "%08x %08x %08x %08x\n" $d0 $d1 $(($d0 / $d1)) $(($d0 % $d1));
 done
 
-for a in $(seq 0 4095); do
+for a in $(seq 0 32767); do
     d0=$RANDOM$RANDOM;
-    d1=$RANDOM;
+    d1=$(($RANDOM + 1));
     printf "%08x %08x %08x %08x\n" $d0 $d1 $(($d0 / $d1)) $(($d0 % $d1));
 done
